ca7a1e08500b318b0451c471c575b59196d82584
Vladimir <izhur27@gmail.com>
2023-02-19 02:07:20 +1100

    initial bootstrap
    asd

--- src/panels/myTickets/index.tsx
+++ src/panels/myTickets/index.tsx
@@ -6,32 +6,64 @@ import {
   Group,
   CardGrid,
   Card,
-  PanelHeaderBack,
-  PanelHeader
+  Div,
+  ModalRoot,
+  Text,
+  CardScroll,
+  SimpleCell,
+  Image,
+  PanelHeader,
+  Spacing,
+  Cell
 } from "@vkontakte/vkui";
-import "./index.css";
+import wallabyPic from "./wallaby.png";
 
 interface IHomeProps {
   id: string;
-  onBack: () => void;
 }
 
-const MyTickets: React.FC<IHomeProps> = ({ id, onBack }) => {
+const MyTickets: React.FC<IHomeProps> = ({ id }) => {
   return (
     <Panel id={id}>
-      <PanelHeader
-        before={
-          <PanelHeaderBack
-            style={{ borderRadius: 0!, boxShadow: "unset"! }}
-            onClick={onBack}
-          />
-        }
-      >
-        Ваши билеты
-      </PanelHeader>
-      <Group mode="plain">
-        <CardGrid size="l">
-          <Card className="card_wrap">Ваши билеты</Card>
+      <Group style={{ paddingBottom: 100 }}>
+        <CardGrid style={{ background: "none", marginTop: 60 }}>
+          {/*<PanelHeader>Ваши билеты</PanelHeader>*/}
+          <CardScroll size="l" style={{ width: 900, height: 100 }}>
+            <Card
+              mode="shadow"
+              style={{ backgroundColor: "white", width: 450 }}
+            >
+              <Cell before={<Image src={wallabyPic} size={96} />}>
+                <Text weight="2">Wallaby Feeding</Text>
+                <Spacing size={6} />
+                <Text>24.03.2023 18:00</Text>
+                <Spacing size={2} />
+                <Text>Melbourne, Docklands</Text>
+                <Spacing size={6} />
+                <Text weight="2">Early Bird VIP</Text>
+              </Cell>
+              <Div style={{ margin: 12, paddingLeft: 18 }}>
+                ID билета: 0xfsdke34fo5ppsdfyw8q
+              </Div>
+            </Card>
+            <Card
+              mode="shadow"
+              style={{ backgroundColor: "white", width: 450 }}
+            >
+              <Cell before={<Image src={wallabyPic} size={96} />}>
+                <Text weight="2">Wallaby Feeding</Text>
+                <Spacing size={6} />
+                <Text>24.03.2023 18:00</Text>
+                <Spacing size={2} />
+                <Text>Melbourne, Docklands</Text>
+                <Spacing size={6} />
+                <Text weight="2">Early Bird VIP</Text>
+              </Cell>
+              <Div style={{ margin: 12, paddingLeft: 18 }}>
+                ID билета: 0xfsdke34fo5ppsdfyw8q
+              </Div>
+            </Card>
+          </CardScroll>
         </CardGrid>
       </Group>
     </Panel>
